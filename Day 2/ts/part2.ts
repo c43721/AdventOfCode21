48dac1ca2ad0df5320d1348120d9e370d4c0bb1b
c43721 <rlahman.game@gmail.com>
2021-12-02 13:21:03 -0600

    feat: complete day 2 part 2

--- Day 2/ts/part2.ts
+++ Day 2/ts/part2.ts
@@ -3,9 +3,9 @@ const text = await Deno.readTextFile("./input.txt");
 type CommandType = "forward" | "up" | "down";
 type Command = `${CommandType} ${string}`;
 
-const commands = text.split('\n') as Command[];
+const commands = text.split("\n") as Command[];
 
-const position = { horizonal: 0, depth: 0 };
+const position = { horizonal: 0, depth: 0, aim: 0 };
 
 commands.forEach((cmd) => mapCommandToPosition(cmd));
 
@@ -15,22 +15,25 @@ function mapCommandToPosition(command: Command) {
   // need me some js pattern matching rn
   switch (direction as CommandType) {
     case "up": {
-      position.depth -= Number(amount);
+      position.aim -= Number(amount);
       break;
     }
     case "down": {
-      position.depth += Number(amount);
+      position.aim += Number(amount);
       break;
     }
     case "forward": {
-      position.horizonal += Number(amount);
+      const amt = Number(amount);
+
+      position.horizonal += amt;
+      position.depth += position.aim * amt;
       break;
     }
   }
 }
 
 console.log(
-  `Horizontal: ${position.horizonal}\tDepth: ${position.depth}\n\tTotal: ${
-    position.depth * position.horizonal
-  }`
+  `Horizontal: ${position.horizonal}\tDepth: ${position.depth}\tAim: ${
+    position.aim
+  }\n\tTotal: ${position.depth * position.horizonal}`
 );
